c31a5764abacb316d8c2db15eb7f4e307c5a37a1
dias1397 <joao.dias1397@gmail.com>
2024-05-21 18:09:36 +0100

    Adds transitions in homepage activity

--- app/src/main/java/com/diasjoao/metrosultejo/activities/HomepageActivity.java
+++ app/src/main/java/com/diasjoao/metrosultejo/activities/HomepageActivity.java
@@ -1,5 +1,6 @@
 package com.diasjoao.metrosultejo.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.diasjoao.metrosultejo.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -18,12 +20,35 @@ public class HomepageActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
 
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        MaterialCardView searchCardView = findViewById(R.id.search_card);
+        searchCardView.setOnClickListener(view -> {
+            Intent intent = new Intent(HomepageActivity.this, RealTimingActivity.class);
+            startActivity(intent);
+        });
+
+        MaterialCardView line1CardView = findViewById(R.id.line_card_1);
+        line1CardView.setOnClickListener(view -> {
+            Intent intent = new Intent(HomepageActivity.this, RoutesActivity.class);
+            startActivity(intent);
+        });
+
+        MaterialCardView line2CardView = findViewById(R.id.line_card_2);
+        line2CardView.setOnClickListener(view -> {
+            Intent intent = new Intent(HomepageActivity.this, RoutesActivity.class);
+            startActivity(intent);
+        });
+
+        MaterialCardView line3CardView = findViewById(R.id.line_card_3);
+        line3CardView.setOnClickListener(view -> {
+            Intent intent = new Intent(HomepageActivity.this, RoutesActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
