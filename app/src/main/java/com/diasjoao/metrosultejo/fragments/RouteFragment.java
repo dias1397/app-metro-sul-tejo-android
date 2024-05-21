746bb4311606239433f085e6d27549d2f1a78567
dias1397 <joao.dias1397@gmail.com>
2024-05-21 17:40:52 +0100

    Ads new routes activity

---
+++ app/src/main/java/com/diasjoao/metrosultejo/fragments/RouteFragment.java
@@ -0,0 +1,21 @@
+package com.diasjoao.metrosultejo.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.diasjoao.metrosultejo.R;
+
+public class RouteFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_route, container, false);
+    }
+}
