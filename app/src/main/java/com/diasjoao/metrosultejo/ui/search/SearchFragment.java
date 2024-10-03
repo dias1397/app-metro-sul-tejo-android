300895f9e095f7b70ee8d4286bad5173dac0e0fb
dias1397 <joao.dias1397@gmail.com>
2024-10-03 18:23:45 +0100

    format SearchFragment.java

--- app/src/main/java/com/diasjoao/metrosultejo/ui/search/SearchFragment.java
+++ app/src/main/java/com/diasjoao/metrosultejo/ui/search/SearchFragment.java
@@ -3,9 +3,6 @@ package com.diasjoao.metrosultejo.ui.search;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +10,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
 
 import com.diasjoao.metrosultejo.R;
 import com.diasjoao.metrosultejo.ui.live.LiveActivity;
