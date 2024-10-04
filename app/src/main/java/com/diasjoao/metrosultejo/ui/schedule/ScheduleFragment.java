cb6acbd1da891d9f34c578f5b14d6777acdf8cfc
dias1397 <joao.dias1397@gmail.com>
2024-10-04 17:20:43 +0100

    format ScheduleFragment.java

--- app/src/main/java/com/diasjoao/metrosultejo/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/diasjoao/metrosultejo/ui/schedule/ScheduleFragment.java
@@ -1,49 +1,60 @@
 package com.diasjoao.metrosultejo.ui.schedule;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.diasjoao.metrosultejo.R;
 import com.diasjoao.metrosultejo.adapters.TimetableAdapter;
 import com.diasjoao.metrosultejo.data.repository.ScheduleRepository;
-import com.diasjoao.metrosultejo.data.model.Station;
-
-import java.util.List;
 
 public class ScheduleFragment extends Fragment {
 
     private RecyclerView recyclerView;
-    private ScheduleRepository scheduleRepository;
 
-    private int seasonId, dayId, lineId = 1;
+    private int seasonId = 1;
+    private int dayId = 1;
+    private int lineId = 1;
+    private TimetableAdapter timetableAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_schedule, container, false);
 
+        initVars();
+        initViews(view);
+
+        setupUI();
+
+        return view;
+    }
+
+    private void initVars() {
         if (getArguments() != null) {
             seasonId = getArguments().getInt("seasonId", 1);
             dayId = getArguments().getInt("dayId", 1);
             lineId = getArguments().getInt("lineId", 1);
         }
 
+        ScheduleRepository scheduleRepository = new ScheduleRepository(requireContext());
+        timetableAdapter = new TimetableAdapter(
+                scheduleRepository.findStationsBySeasonAndDayAndLine(seasonId, dayId, lineId)
+        );
+    }
+
+    private void initViews(View view) {
         recyclerView = view.findViewById(R.id.recyclerView);
+    }
+
+    private void setupUI() {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-
-        scheduleRepository = new ScheduleRepository(requireContext());
-
-        List<Station> stations = scheduleRepository.findStationsBySeasonAndDayAndLine(seasonId, dayId, lineId);
-        recyclerView.setAdapter(new TimetableAdapter(stations));
-
-        return view;
+        recyclerView.setAdapter(timetableAdapter);
     }
 }
